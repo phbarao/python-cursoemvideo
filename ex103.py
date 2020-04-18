f14b866d687a720a81b7caf3bcc4ee65c7111c31
Pedro Barão <phbarao@gmail.com`>
2020-04-17 23:05:11 -0300

    adicionando ex102 até o ex106

---
+++ ex103.py
@@ -0,0 +1,16 @@
+def ficha(jog='<desconhecido>', gol=0):
+    print(f'O jogador {jog} fez {gol} gol(s) no campeonato.')
+
+
+#Programa Principal
+nome = str(input('Nome do Jogador: '))
+gols = str(input('Número de Gols: '))
+
+if gols.isnumeric:
+    gols = int(gols)
+else:
+    gols = 0
+if nome.strip() == '':
+    ficha(gol=gols)
+else:
+    ficha(nome, gols)
